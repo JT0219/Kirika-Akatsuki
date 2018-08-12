bfb1901c8975fadb99a47361e88b4946e9885aec
JT0219 <40906876+JT0219@users.noreply.github.com>
2018-08-12 23:09:13 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,69 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client();
+
+bot.on('message', (message) => {
+
+    if(message.author.bot) return;
         if(message.content == 'Good morning, Kirika!'){
-            member.guild.channels.get('373571542769991681').send('Good morning, dess!');
+        message.channel.send('Good morning, dess!');
     }
     if(message.content == 'Good evening, Kirika!'){
-        member.guild.channels.get('373571542769991681').send('Good evening, dess!');
+        message.channel.send('Good evening, dess!');
     }
     if(message.content == 'Dess!'){
-        member.guild.channels.get('373571542769991681').send('Dess!');
+        message.channel.send('Dess!');
     }
     if(message.content == 'Dess'){
-        member.guild.channels.get('373571542769991681').send('pacito!');
+        message.channel.send('pacito!');
     }
     if(message.content == 'Hello, Kirika!'){
-        member.guild.channels.get('373571542769991681').send('Hello, dess!');
+        message.channel.send('Hello, dess!');
     }
     if(message.content == 'Goodbye, Kirika!'){
-        member.guild.channels.get('373571542769991681').send('Bye-bye, dess!');
+        message.channel.send('Bye-bye, dess!');
     }
     if(message.content == 'Who is the best couple in anime?'){
-        member.guild.channels.get('373571542769991681').send('DMJii of course, dess!');
+        message.channel.send('DMJii of course, dess!');
     }
     if(message.content == 'Dess or Death?'){
-        member.guild.channels.get('373571542769991681').send('Death, of co-I mean, dess, of course, dess!');
+        message.channel.send('Death, of co-I mean, dess, of course, dess!');
     }
     if(message.content == 'Dess?'){
-        member.guild.channels.get('373571542769991681').send('?');
+        message.channel.send('?');
     }
     if(message.content == 'DESS!!!!'){
-        member.guild.channels.get('373571542769991681').send('Dont shout at me, dess!');
+        message.channel.send('Dont shout at me, dess!');
     }
-    if(message.content == 'Can I ping you?'){
-        member.guild.channels.get('373571542769991681').send('Dont you dare ping me, dess.');
+        if(message.content == 'Can I ping you?'){
+        message.channel.send('Dont you dare ping me, dess.');
     }
     if(message.content == 'Kongou Dess!'){
-        member.guild.channels.get('373571542769991681').send('Kongou what, dess?');
+        message.channel.send('Kongou what, dess?');
     }
     if (message.mentions.users.has('464722512169795594')){
-        member.guild.channels.get('373571542769991681').send('Did you just fucking ping me, dess?');
+        message.channel.send('Did you just fucking ping me, dess?');
     }
     if(message.content == 'Who are you?'){
-        member.guild.channels.get('373571542769991681').send('Im Kirika, dess!');
+        message.channel.send('Im Kirika, dess!');
     }
     if(message.content == 'Desu!'){
-        member.guild.channels.get('456297725223501825').send('That is not the right spelling, dess.');
+        message.channel.send('That is not the right spelling, dess.');
     }
     if(message.content == 'Say Shirabe'){
-        member.guild.channels.get('373571542769991681').send('My love!');
+        message.channel.send('My love!');
     }
     if(message.content == 'です!'){
-        member.guild.channels.get('373571542769991681').send('What the fuck are you saying, dess?');
+        message.channel.send('What the fuck are you saying, dess?');
     }
 
+});
+
+bot.on('guildMemberAdd', member => {
+    member.guild.channels.get('456297725223501825').send('**' + member.user.username + '** has joined the server, dess! I hope this user enjoys the server, dess!!');
+});
+
+bot.on('guildMemberRemove', member => {
+    member.guild.channels.get('456297725223501825').send('**' + member.user.username + '** has left the server, dess. I will now cry on my corner, dess. T___T');
 });
 
 bot.login(process.env.BOT_TOKEN);
